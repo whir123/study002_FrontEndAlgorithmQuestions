b8776f93ebaf9d238642c4a1cce1563cad7e155b
whir123 <1549820081@qq.com>
2026-01-24 02:00:23 +0800

    260124

--- 8_LeetCode每日一题2026/260122_移除最小数对使数组有序.js
+++ 8_LeetCode每日一题2026/260122_移除最小数对使数组有序.js
@@ -8,5 +8,39 @@
  */
 
 var minimumPairRemoval = function(nums) {
-    
+    let opt = 0;
+
+    function isOK(arr){
+        for (let i=1; i<arr.length; i++){
+            if(arr[i-1]>arr[i]) return false;
+        };
+        return true;
+    };
+
+    while (!isOK(nums)){
+        let minSum = Infinity;
+        let idx = 0;
+        for (let i=0; i<nums.length-1; i++){
+            const s = nums[i]+nums[i+1];
+            if (s<minSum){
+                minSum = s;
+                idx = i;
+            };
+        };
+        nums.splice(idx, 2, nums[idx]+nums[idx+1]); // 在idx处，移除2个元素，插入nums[idx]+nums[idx+1]
+        opt++;
+    };
+
+    return opt;
 };
+
+const nums1 = [5,2,3,1];
+console.log(minimumPairRemoval(nums1));
+// 输出： 2
+// 元素对 (3,1) 的和最小，为 4。替换后 nums = [5,2,4]。
+// 元素对 (2,4) 的和为 6。替换后 nums = [5,6]。
+// 数组 nums 在两次操作后变为非递减。
+
+const nums2 = [1,2,2];
+console.log(minimumPairRemoval(nums2));
+// 输出： 0
